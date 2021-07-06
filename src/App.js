71271df1f69cdb67e1e3091db766fe770454657d
Toai-1995 <donguyentoai1995@gmail.com>
2021-07-06 16:56:19 +0700

    login with google

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Login from './components/Login';
+import Chatroom from './components/ChatRoom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/' component={Chatroom} />
+      </Switch>
+    </BrowserRouter>
+    // <div className="App">
+    //   <Login></Login>
+    // </div>
   );
 }
 
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { Row, Col, Button, Typography } from 'antd';
 import firebase, { auth } from '../../firebase/config'
+import { useHistory } from 'react-router-dom';
 
 const { Title } = Typography
 const fbProvider = new firebase.auth.FacebookAuthProvider();
-export default function index() {
+const ggProvider = new firebase.auth.GoogleAuthProvider();
+
+export default function Login() {
+  const history = useHistory();
   const loginFacebook = () => {
     console.log('click');
-    auth.signInWithPopup(fbProvider)
+    auth.signInWithPopup(fbProvider);
   }
+  const loginGoogle = () => {
+    auth.signInWithPopup(ggProvider);
+  }
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      history.push('/')
+    }
+  })
   return (
     <div>
       <Row justify='center' style={{ height: 800 }}>
         <Col span={8}>
           <Title style={{ textAlign: 'center' }}>Đăng Nhập</Title>
-          <Button style={{ width: '100%', marginBottom: 5 }}>Đăng nhập bằng Google</Button>
+          <Button style={{ width: '100%', marginBottom: 5 }} onClick={loginGoogle}>Đăng nhập bằng Google</Button>
           <Button style={{ width: '100%', marginBottom: 5 }} onClick={loginFacebook}>Đăng nhập bằng FaceBook</Button>
         </Col>
       </Row>
